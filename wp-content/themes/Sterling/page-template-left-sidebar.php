907f00ce383d6fc9d2243d38771a191a53ed32f9
Nicholas <zuzionly@gmail.com>
2014-03-08 22:33:14 +0800

    update color

--- wp-content/themes/Sterling/page-template-left-sidebar.php
+++ wp-content/themes/Sterling/page-template-left-sidebar.php
@@ -10,7 +10,26 @@ get_template_part( 'template-part-page-slider', 'childtheme' ); ?>
 <section id="content-container" class="clearfix">
     <div id="main-wrap" class="clearfix">
         <aside class="sidebar left-sidebar">
-            <?php generated_dynamic_sidebar(); ?>
+            <div class="subnav">
+                <?php
+                    global $post;
+                    $custom_menu_slug = get_post_meta( $post->ID, 'truethemes_custom_sub_menu', true );
+
+                    if ( empty( $custom_menu_slug ) ) :
+                        wp_nav_menu( array( 'container' => false, 'theme_location' => 'Main Menu', 'walker' => new truethemes_sub_nav_walker() ) );
+                    else :
+                        echo '<ul>';
+                            wp_nav_menu( array( 'container' => false, 'menu' => $custom_menu_slug, 'walker' => new truethemes_sub_nav_walker_two() ) );
+                        echo '</ul>';
+                    endif;
+                ?>
+            </div><!-- end subnav -->
+
+            <?php // Check for selected sidebar, does not print anything if no sidebar has been selected.
+                $selected_sidebar = get_post_meta( $post->ID, 'sbg_selected_sidebar_replacement', true );
+                if ( ! empty( $selected_sidebar[0] ) )
+                    generated_dynamic_sidebar();
+            ?>
         </aside><!-- end sidebar-->
         <div class="page_content_right sub-content">
             <?php
